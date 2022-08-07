9964007922676623b4e4ff566c5b5ae952487084
Breno Farias <brenopalheta1999@gmail.com>
2022-08-07 11:21:41 -0300

    implementando operações
    - Adicionado verificações para denominador divisão seja sempre diferente de zero

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,41 @@
 function calcular(num1, num2){
 
     //pegando os valores das caixas de texto num1 e num2
-    num1 = document.getElementById("num1").value
-    num2 = document.getElementById("num2").value
+    num1 = Number.parseFloat(document.getElementById("num1").value)
+    num2 = Number.parseFloat(document.getElementById("num2").value)
 
     selector = document.getElementById("selector").value
     
-    switch(selector){
+    switch(selector)
+    {
         case "+":
             calculo = (num1 + num2)
             document.getElementById("resultado").innerHTML = `O resultado da soma é ${calculo}`
             break
+
         case "-":
             calculo = (num1 - num2)
             document.getElementById("resultado").innerHTML = `O resultado da subutração é ${calculo}`
             break
+
+        case "/":
+            calculo = (num1 / num2)
+            if(num2 != 0 ) // para limitar denomidador igual a zero
+            {
+                document.getElementById("resultado").innerHTML = `O resultado da subutração é ${calculo}`
+            } 
+            else
+            {
+                alert("Insira um valor diferente de zero no denominador (segundo número)")
+            }
+            break
+
+        case "*":
+            calculo = (num1 * num2)
+            document.getElementById("resultado").innerHTML = `O resultado da subutração é ${calculo}`
+            break
+
+        default:
+            alert("Desculpe, houve algum erro inesperado")
     }
 }
